646a14503444768da3981b9213d0d85a33380808
kimoban <67864381+kimoban@users.noreply.github.com>
2025-07-02 11:44:44 +0000

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -12,6 +12,8 @@ JOIN users u ON b.user_id = u.id
 JOIN properties p ON b.property_id = p.id
 LEFT JOIN payments pay ON b.id = pay.booking_id;
 
+["WHERE", "AND"]
+    
 -- Optimized query
 SELECT 
     b.*,
